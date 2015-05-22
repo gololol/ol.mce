f3852e39e321d2e8fa4ca451fc9f1e29620ec6d6
gololol <gololol@ukr.net>
2015-05-22 16:00:23 +0400

    hello

---
+++ app/code/community/Markshust/Helloworld/controllers/indexController.php
@@ -0,0 +1,9 @@
+<?php
+class Markshust_Helloworld_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "<h1>"."Hello world!"."</h1>";
+        echo "<h1>"."Hello world!"."</h1>";
+    }
+}
