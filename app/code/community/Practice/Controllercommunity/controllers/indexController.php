1438cd43b565c16efc7cbd9abcae04b115605f2f
gololol <gololol@ukr.net>
2015-05-22 18:58:46 +0400

    local

---
+++ app/code/community/Practice/Controllercommunity/controllers/indexController.php
@@ -0,0 +1,8 @@
+<?php
+class Practice_Controllercommunity_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "<h1>"."Controller community"."</h1>";
+    }
+}
---
+++ app/code/local/Practice/Controllerlocal/controllers/indexController.php
@@ -0,0 +1,8 @@
+<?php
+class Practice_Controllerlocal_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "<h1>"."Controller local"."</h1>";
+    }
+}
